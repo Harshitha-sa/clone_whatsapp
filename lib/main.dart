d4055705ad7d7c0236c8185bd2e41bb204a24232
Harshitha <saharshithasa@gmail.com>
2019-11-02 15:01:39 +0530

    anything

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>  with SingleTickerProviderStateMixin {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+  TabController controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new TabController(
+      length: 3,
+      initialIndex: 0,
+      vsync: this,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    Color whatsAppColor = Color(0xFF32642E);
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            Tab(child: Text("chats"),),
+            Tab(child: Text("status"),),
+            Tab(child: Text("calls"),),
+          ],
+        ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search),
+            color: Colors.white,
+            onPressed: (){
+              //TODO
+            },),
+          IconButton(icon: Icon(Icons.more_vert),
+            onPressed: (){
+              //TODO
+            },)
+        ],
+        backgroundColor: whatsAppColor,
+        title: Text('WhatsApp'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          //TODO
+
+        },
+        child: Icon(Icons.chat),
+        backgroundColor: whatsAppColor,
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int count) {
+            return WhatsAppCard();
+          }),
+    );
+  }
+}
+
+class WhatsAppCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 95.0,
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: Icon(
+              Icons.account_circle,
+              size: 75.0,
+              color: Colors.grey.withOpacity(0.7),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'Harshitha',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.check,
+                      size: 15.0,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      'Hi!',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 14.0),
+            child: Text('15:32'),
+          ),
+        ],
+      ),
+    );
+  }
+}
